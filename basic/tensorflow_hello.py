309a6857ad104d061c5dc41761ed1ccc761b3b4a
kosooyoul <kosooyoul@gmail.com>
2020-08-06 10:53:10 +0900

    add tensorflow hello

---
+++ basic/tensorflow_hello.py
@@ -0,0 +1,23 @@
+# First, should install conda (check options env var)
+# Second, update env and create virtual env
+# > conda update -n base conda
+# > conda --all
+# > python -m pip install --upgrade pip
+# > conda create -n tensorflow python=3.6
+# > conda activate tensorflow
+# Third, install tensorflow v1.5
+# > (tensorflow) > pip install tensorflow==1.5
+# Other Third, install tensorflow by conda
+# > (tensorflow) > conda install tensorflow
+
+import tensorflow as tf
+
+print('# Start')
+
+hello = tf.constant('Hello Tensorflow')
+
+session = tf.Session()
+
+print(session.run(hello))
+
+print('# End')
